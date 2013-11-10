517b623bcab7ec50b3a117d7c5b3e3a2dfe1621f
mariabg <mariabenaventeg@gmail.com>
2013-11-10 16:40:34 +0100

    m_mapOnLocation

--- isabelgomez/js/script(L).js
+++ isabelgomez/js/script(L).js
@@ -17,6 +17,7 @@ $(document).ready(function(){
 		$("#indexMenu, #aboutMenu, #contactMenu").removeClass("activeLink").addClass("NonActiveLink");
 		$("#location").removeClass("hide").addClass("show");
 		$("#index, #about, #contact").removeClass("show").addClass("hide");
+		initialize();
 	});
 	$("#contactMenu").click(function(){
 		$(this).removeClass("nonActiveLink").addClass("activeLink");
@@ -25,3 +26,39 @@ $(document).ready(function(){
 		$("#index, #about, #location").removeClass("show").addClass("hide");
 	});
 });
+
+//This code comes from--> http://jsfiddle.net/aCx6L/ it's f**king brilliant
+   function initialize() {
+        var myLatLng = new google.maps.LatLng(42.2377, -8.7148336);
+        var mapCanvas = document.getElementById('mapCanvas');
+        var mapOptions = {
+        center: new google.maps.LatLng(42.2377, -8.7148336),
+        zoom: 15,
+	        mapTypeControl: false,
+	        panControl: false,
+	        zoomControlOptions: {
+	                    position: google.maps.ControlPosition.LEFT_CENTER
+			},
+	        streetViewControl: false,
+	        mapTypeId: google.maps.MapTypeId.ROADMAP
+	    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+        var contentString =
+            '<b>Isabel Gómez, psicóloga</b>' +
+            '<p>52 Rosalía de Castro, Vigo</p>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+        });
+
+        google.maps.event.trigger(map, 'resize');
+
+        google.maps.event.addListener(marker, 'click', function(){
+            infowindow.open(map,marker);
+        });
+    }
